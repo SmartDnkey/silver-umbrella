e5afd75d5738067e0dfa2692e6239f2b64651243
Nil Torres <nil.torres23@gmail.com>
2023-06-26 22:55:00 +0200

    Fixed html delivery for root path

--- apiREST/apiREST/settings.py
+++ apiREST/apiREST/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'apiREST.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,22 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "/app/info.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
--- apiREST/apiREST/views.py
+++ apiREST/apiREST/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def main(request):
-    return HttpResponse("Hello, world! This is a simple Django app")
+    return render(request, 'main.html')
